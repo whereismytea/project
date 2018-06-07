637e34d0b2c39d0130ffd6a2346eae23c88e9e37
Andrey Eltsov <andrei.eltzov@gmail.com>
2018-06-07 16:31:52 +0300

    возвращен HurtEnemy.cs

--- Assets/Scripts/HurtEnemy.cs
+++ Assets/Scripts/HurtEnemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HurtEnemy : MonoBehaviour {
 
@@ -9,16 +10,26 @@ public class HurtEnemy : MonoBehaviour {
 	public GameObject damageBurst;
 
 	private PlayerStats thePS;
-
+	public Text damageInfo;
+	public float timeToHide = 2.0f;
 
 	// Use this for initialization
 	void Start () {
 		thePS = FindObjectOfType<PlayerStats> ();
+		damageInfo = GameObject.Find ("DamageInfo").GetComponent<Text> ();
+		damageInfo.enabled = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (damageInfo.enabled) {
+			timeToHide -= Time.deltaTime;
+			if (timeToHide < 0.0f) {
+				timeToHide = 2.0f;
+				damageInfo.enabled = false;
+			}
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
@@ -27,8 +38,11 @@ public class HurtEnemy : MonoBehaviour {
 			//Destroy (other.gameObject); 
 			currentDamage = damageToGive + thePS.currentAttack; 
 			other.gameObject.GetComponent<EnemyHealthManager>().HurtEnemy(damageToGive);
-			Instantiate (damageBurst, transform.position, transform.rotation);
+			Instantiate (damageBurst, GameObject.Find("swing big").transform.position, GameObject.Find("swing big").transform.rotation);
+			damageInfo.enabled = true;
+			damageInfo.text = "-" + currentDamage.ToString ();
+			timeToHide = 2.0f;
+	
 		}
-
 	}
 }
